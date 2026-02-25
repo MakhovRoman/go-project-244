28b8cd773be24c5d1d797aa5042ce5d603c931a3
roman-makhov <roman.makhov@fin-frame.ru>
2026-02-25 13:04:10 +0300

    step_2: add format option

--- cmd/gendiff/main.go
+++ cmd/gendiff/main.go
@@ -11,5 +11,13 @@ func main() {
 	(&cli.Command{
 		Name:  "gendiff",
 		Usage: "Compares two configuration files and shows a difference.",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "format",
+				Aliases: []string{"f"},
+				Value:   "stylish",
+				Usage:   "output format",
+			},
+		},
 	}).Run(context.Background(), os.Args)
 }
